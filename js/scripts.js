69cb45e75903cd81de2fb402e14e2fa31a6b0578
Mário Veronesi <marioinfo@oi.com.br>
2019-02-13 12:59:12 -0200

    Primeiro Commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,8 @@
 /*SCRIPTS HERE*/
 $(document).ready(function(e) {
-	/******************* ITENS ********************/
+	/******************* MENU ********************/
+	$(".toggle-menu").click(function(){
+		$(this).find("i").toggleClass('hidden');
+		$("#container-menu").slideToggle();
+	});
 });
